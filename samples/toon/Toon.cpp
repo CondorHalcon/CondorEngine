7b01fbf74dc62853de1a605fbe5933bc2f71b27f
CondorHalcon <judahhohawi@outlook.com>
2025-03-15 17:06:47 -0700

    Added Toon Sample executable

---
+++ samples/toon/Toon.cpp
@@ -0,0 +1,13 @@
+#include <CondorEngine.hpp>
+#include <stdexcept>
+
+using namespace CondorEngine;
+
+class ToonMat : public Material {};
+
+class ToonScene : public Scene {};
+
+int main() {
+    Debug::Log("Toon Sample");
+    return 0;
+}
